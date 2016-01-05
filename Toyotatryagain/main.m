5272d54d4d64afb0a57cd443eace37e205584d21
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-04 22:04:23 -0500

    Initial Commit

---
+++ Toyotatryagain/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Toyotatryagain
+//
+//  Created by Kerry Toonen on 2016-01-04.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
